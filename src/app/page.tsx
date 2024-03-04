addef3e22a8efe0ff91278299e9bbaa6cfb93fa1
sawoaj <126136670+sawoaj@users.noreply.github.com>
2024-03-04 21:55:08 +0530

    renamed files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Hello World</h1>
+			<h1>Please go to http://localhost:3000/login for login page</h1>
 		</main>
 	);
 }
